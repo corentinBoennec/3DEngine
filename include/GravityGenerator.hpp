1b198e012f552e818ce2512ea0ea69ac967966f2
Clément Poisson <cmt.poisson@outlook.com>
2018-09-28 18:51:24 -0400

    GravityGenerator

--- include/GravityGenerator.hpp
+++ include/GravityGenerator.hpp
@@ -6,14 +6,6 @@ class GravityGenerator : public ParticuleForceGenerator
 private:
 	Vector3D gravity;
 public:
-<<<<<<< HEAD
 	~GravityGenerator();
 	GravityGenerator(Vector3D gravity);
 	void updateForce(Particule* particule);
-=======
-	GravityGenerator(Vector3D gravity);
-	~GravityGenerator();
-	void updateForce(Particule * particule, float time);
-
->>>>>>> 7d1e2ad34ecc19a9b2d26c89fdbb521659dcd761
-};
